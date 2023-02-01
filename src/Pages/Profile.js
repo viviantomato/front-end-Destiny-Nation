b64ff0ed9bfa8070559b9f55d24f1de2a5182fcc
viviantomato <vivianyw924@gmail.com>
2023-01-31 19:00:35 -0800

    added footprint table with material UI

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./Profile.css";
+import "../Components/Footprint";
+import "../Components/Wishlist";
+
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Footprint from "../Components/Footprint";
 
 const Profile = () => (
   <div>
     <h1>Profile ⌨️ </h1>
     <main>
-      <h2>Bio</h2>
+      <h2>Bio 🎴 </h2>
+      <TextField id="outlined-basic" label="Who you are" variant="outlined" />
+
+      <Button variant="contained">Connect</Button>
+      <Button variant="contained">Message</Button>
+
+      <h2>Footprint 🐾 </h2>
+      <Footprint></Footprint>
+
+      <h2>Wishlist ✨ </h2>
     </main>
   </div>
 );
